a1a694bdb854ceb40fc4e6d77af5026ee397cc64
Jacob Steves <jgs1998@hotmail.com>
2020-12-29 09:50:20 -0500

    validate required arguments

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-if [ -n $INPUT_TARBALL ]; then
+if [[ -z ${INPUT_SERVER} ]] || [[ -z ${INPUT_PASSWORD} ]] || [[ -z ${INPUT_APPNAME} ]]; then
+  echo "Invalid input. Some required arguments missing."
+  exit 1
+fi
+
+if [[ ! -z ${INPUT_TARBALL} ]]; then
   caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -a $INPUT_APPNAME -t $INPUT_TARBALL
-elif [ -n $INPUT_BRANCH ]; then
+elif [[ ! -z ${INPUT_BRANCH} ]]; then
   caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -a $INPUT_APPNAME -b $INPUT_BRANCH
 else
   echo "Invalid input. Either a tarball or a branch argument is required."
